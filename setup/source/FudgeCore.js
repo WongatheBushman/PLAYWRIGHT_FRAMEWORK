d573170a023d0b510f7fefa95f5a935b7af3e935
WongatheBushman <82675799+WongatheBushman@users.noreply.github.com>
2026-03-31 14:37:02 +0200

    Update FudgeCore.js

--- setup/source/FudgeCore.js
+++ setup/source/FudgeCore.js
@@ -1,4 +1,4 @@
-"use strict";//
+"use strict";
 var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
     var useValue = arguments.length > 2;
     for (var i = 0; i < initializers.length; i++) {
